bbc1b4e60754a3e5060d134ca201147baf85e9e4
HellOwhatAs <xjq701229@outlook.com>
2023-04-12 23:29:57 +0800

    better segment

--- 大作业/main.py
+++ 大作业/main.py
@@ -11,6 +11,18 @@ sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
 mask_generator = SamAutomaticMaskGenerator(sam)
 
+def findVertex(mask: np.ndarray, max_iter = 20):
+    thresh = mask.astype(np.uint8) * 255
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    factor = 0.01
+    for _ in range(max_iter):
+        epsilon = factor * cv2.arcLength(contours[0], True)
+        approx = cv2.approxPolyDP(cv2.convexHull(contours[0]), epsilon, True)
+        if len(approx) == 4: return approx
+        if len(approx) > 4: factor += 0.005
+        else: factor -= 0.005
+    raise Exception("beyound max_iter")
+
 def segment(image: np.ndarray):
     masks = mask_generator.generate(image)
     max_img, max_val, max_idx, max_part, max_mask = None, 0, None, None, None
@@ -19,12 +31,15 @@ def segment(image: np.ndarray):
         if part < 0.005: continue
         mask_img = np.where(ann['segmentation'][:,:,None][:,:,(0,0,0)], image, 0)
         mask_hsv = cv2.cvtColor(mask_img, cv2.COLOR_BGR2HSV)
-
         val = max(
-            (np.sum(cv2.inRange(mask_hsv, np.array([105, 161, 108]), np.array([112, 255, 210])) != 0) / np.sum(ann['segmentation'] != 0)),
+            (np.sum(cv2.inRange(mask_hsv, np.array([105, 161, 108]), np.array([112, 255, 255])) != 0) / np.sum(ann['segmentation'] != 0)),
             (np.sum(cv2.inRange(mask_hsv, np.array([38, 30, 133]), np.array([100, 164, 218])) != 0) / np.sum(ann['segmentation'] != 0))
         )
         if val > 0.7: continue
+
+        try:findVertex(ann['segmentation'])
+        except:continue
+
         if val > max_val:
             max_val = val
             max_img = mask_img
@@ -32,15 +47,6 @@ def segment(image: np.ndarray):
             max_part = part
             max_mask = ann['segmentation']
     return max_mask
-
-
-def findVertex(mask: np.ndarray):
-    thresh = mask.astype(np.uint8) * 255
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    epsilon = 0.03 * cv2.arcLength(contours[0], True)
-    approx = cv2.approxPolyDP(cv2.convexHull(contours[0]), epsilon, True)
-    assert len(approx) == 4
-    return approx
 
 if __name__ == '__main__':
     for impath in (
